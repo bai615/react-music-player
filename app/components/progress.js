bae673f96b453185962f773bfed54d7c961a4b7b
baihua@sndskj.com <baihua2018>
2018-04-19 18:02:24 +0800

    自定义进度计算 test

--- app/components/progress.js
+++ app/components/progress.js
@@ -2,9 +2,15 @@ import React from 'react'
 import './progress.less'
 
 class Progress extends React.Component {
+    changeProgress(e) {
+        let progressBar = this.refs.progressBar;
+        let progress = (e.clientX - progressBar.getBoundingClientRect().left) / progressBar.clientWidth
+        console.log(progress);
+    }
+
     render() {
         return (
-            <div className="component-progress row">
+            <div className="component-progress row" ref="progressBar" onClick={this.changeProgress}>
                 <div className="progress" style={{width: `${this.props.progress}%`}}></div>
             </div>
         );
