7990397ff6f5c178c3aec1744420195479ab3869
baihua@sndskj.com <baihua2018>
2018-04-20 13:53:35 +0800

    播放器 UI 布局

--- app/page/player.js
+++ app/page/player.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Progress from '../components/progress'
+import './player.less'
 
 class Player extends React.Component {
 
@@ -35,14 +36,50 @@ class Player extends React.Component {
 
     render() {
         return (
-            <div>
-                <Progress
-                    progress={this.state.progress}
-                    onProgressChange={this.progressChangeHandler}
-                    barColor="#ff0000"
-                ></Progress>
+            <div className="player-page">
+                <h1 className="caption">我的私人音乐坊 &gt;</h1>
+                <div className="mt20 row">
+                    <div className="controller-wrapper">
+                        <h2 className="music-title">歌曲名称</h2>
+                        <h3 className="music-artist mt10">歌手</h3>
+                        <div className="row mt20">
+                            <div className="left-time -col-auto">-2:00</div>
+                            <div className="volume-controller">
+                                <i className="icon-volume rt" style={{top: 5,}}></i>
+                                <div className="volume-wrapper">
+                                    音量控制部分
+                                </div>
+                            </div>
+                        </div>
+                        <div style={{height: 10, lineHeight: '10px'}}>
+                            播放进度部分
+                        </div>
+                        <div className="mt35 row">
+                            <div>
+                                <i className="icon prev"></i>
+                                <i className="icon ml20 play"></i>
+                                <i className="icon next ml20"></i>
+                            </div>
+                            <div className="-col-auto">
+                                <i className="icon repeat-cycle"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="-col-auto cover">
+                        <img src="" alt="歌曲名称"/>
+                    </div>
+                </div>
             </div>
         );
+        // return (
+        //     <div className="player-page">
+        //         <Progress
+        //             progress={this.state.progress}
+        //             onProgressChange={this.progressChangeHandler}
+        //             barColor="#ff0000"
+        //         ></Progress>
+        //     </div>
+        // );
     }
 }
 
