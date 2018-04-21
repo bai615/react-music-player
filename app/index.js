2798825fba14c7cdd7ad2917f220f1c109ae4bab
白华 <baihua@baihuadeMBP.lan>
2018-04-21 22:44:14 +0800

    两个页面间切换

--- app/index.js
+++ app/index.js
@@ -1,54 +1,107 @@
 import React from "react";
 import ReactDOM from "react-dom"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Header from './components/header'
+import {MUSIC_LIST} from "./config/musiclist";
+import MusicList from './page/musiclist'
+import Player from "./page/player";
 
 // https://reacttraining.com/react-router/web/example/basic
+// https://github.com/ReactTraining/react-router
+// http://www.ruanyifeng.com/blog/2016/05/react_router.html
 
-class BasicExample extends React.Component{
+class RootComponent extends React.Component{
+    constructor() {
+        super();
+        console.log(MUSIC_LIST);
+        this.state = {
+            musicList: MUSIC_LIST, // 播放列表
+            currentMusicItem: MUSIC_LIST[0] // 当前播放
+        }
+    }
+
+    componentDidMount() {
+        $('#player').jPlayer({
+            ready: function () {
+                $(this).jPlayer('setMedia', {
+                    mp3: 'http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3'
+                }).jPlayer('play');
+            },
+            supplied: 'mp3',
+            wmode: 'window'
+        });
+    }
+
     render(){
         return (
+            <div>
+                <Header/>
             <Router>
                 <div>
                     <ul>
                         <li>
-                            <Link to="/">Home</Link>
+                            <Link to="/">Home (Player)</Link>
                         </li>
                         <li>
-                            <Link to="/about">About</Link>
+                            <Link to="/list">Music List</Link>
                         </li>
 
                     </ul>
 
                     <hr />
 
-                    <Route exact path="/" component={Home} />
-                    <Route path="/about" component={About} />
+                    <Route exact path="/" component={PlayMusicComponent} />
+                    <Route path="/list" component={MusicListComponent} />
 
                 </div>
             </Router>
-        );
-    }
-}
-
-class Home extends React.Component{
-    render(){
-        return (
-            <div>
-                <h2>Home</h2>
             </div>
         );
     }
 }
 
-class About extends React.Component{
+class PlayMusicComponent extends React.Component{
+    constructor() {
+        super();
+        console.log(MUSIC_LIST);
+        this.state = {
+            musicList: MUSIC_LIST, // 播放列表
+            currentMusicItem: MUSIC_LIST[0] // 当前播放
+        }
+    }
     render(){
         return (
             <div>
-                <h2>About</h2>
+                <Player
+                    currentMusicItem={this.state.currentMusicItem}
+                >
+                </Player>
+            </div>
+        );
+    }
+}
+
+class MusicListComponent extends React.Component{
+    constructor() {
+        super();
+        console.log(MUSIC_LIST);
+        this.state = {
+            musicList: MUSIC_LIST, // 播放列表
+            currentMusicItem: MUSIC_LIST[0] // 当前播放
+        }
+    }
+    render(){
+        return (
+            <div>
+                <MusicList
+                    currentMusicItem={this.state.currentMusicItem}
+                    musicList={this.state.musicList}
+                >
+                </MusicList>
             </div>
         );
     };
 }
 
 var oBox = document.getElementById("box");
-ReactDOM.render(<BasicExample/>, oBox);
+ReactDOM.render(<RootComponent/>, oBox);
