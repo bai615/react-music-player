8c1df7ab45db03e008fa179be10fa99eff8b6192
白华 <baihua@baihuadeMBP.lan>
2018-04-21 22:10:48 +0800

    router test ok

--- app/index_ok.js
+++ app/index_ok.js
@@ -2,7 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom"
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-// https://reacttraining.com/react-router/web/example/basic
+/*
+const BasicExample = () => (
+    <Router>
+        <div>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+
+            </ul>
+
+            <hr />
+
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+
+        </div>
+    </Router>
+);
+
+const Home = () => (
+    <div>
+        <h2>Home</h2>
+    </div>
+);
+
+const About = () => (
+    <div>
+        <h2>About</h2>
+    </div>
+);
+*/
 
 class BasicExample extends React.Component{
     render(){
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
+'use strict';
+
+var path = require('path')
+var webpack = require('webpack')
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',// production
-    context:__dirname+"/app", //源文件目录
+    context: path.join(__dirname, "/app"), //源文件目录
     entry:{
         app:"./index.js" //在源文件目录下去找index.js 文件作为打包的入口文件
     },
     output:{
-        path:__dirname+"/dist", //生成的文件存放目录
+        path: path.join(__dirname, "/dist"), //生成的文件存放目录
         filename:"[name].bundle.js" //生成的文件 name 表示entry下面的app
     },
     module:{
